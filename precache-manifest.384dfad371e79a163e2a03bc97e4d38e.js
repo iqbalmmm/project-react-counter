255c7baaf53d93cbf5af2e16c496789d3e782cd0
iqbalmmm <iqbal.itang@gmail.com>
2019-11-15 15:58:11 +0700

    Updates

---
+++ precache-manifest.384dfad371e79a163e2a03bc97e4d38e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff467cce3e23d9c1dd54815d723bbe0",
+    "url": "/project-react-counter/index.html"
+  },
+  {
+    "revision": "b61c4b46f230a2116b0f",
+    "url": "/project-react-counter/static/css/main.c4444dd1.chunk.css"
+  },
+  {
+    "revision": "798f56c21c6f3fb37326",
+    "url": "/project-react-counter/static/js/2.882b9093.chunk.js"
+  },
+  {
+    "revision": "b61c4b46f230a2116b0f",
+    "url": "/project-react-counter/static/js/main.922c4518.chunk.js"
+  },
+  {
+    "revision": "aedfc5c16b1e667983ca",
+    "url": "/project-react-counter/static/js/runtime-main.01c0f892.js"
+  },
+  {
+    "revision": "fce6c2de1eb6d4753eb096abdab5c4f8",
+    "url": "/project-react-counter/static/media/holi.fce6c2de.jpeg"
+  }
+]);
